a6a90c6067b546f6990546f17ead1d660561e965
袁凡迪 <541832074@qq.com>
2017-09-20 02:11:40 +0800

    no message

--- config/load.go
+++ config/load.go
@@ -2,11 +2,8 @@ package tracherConfig
 
 import (
 	"os"
-	"bufio"
-	"io"
-	"strings"
-	"strconv"
 	"log"
+	"fmt"
 )
 
 func Load() (cfg *Config, err error) {
@@ -18,42 +15,6 @@ func Load() (cfg *Config, err error) {
 		cfg.SQL = sql
 	}
 	log.Println(sql)
+	fmt.Println(sql)
 	return cfg,nil
-}
-
-
-func initConfig(cfg *Config,path string) {
-	f, err := os.Open(path)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-
-	r := bufio.NewReader(f)
-	for {
-		b, _, err := r.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			panic(err)
-		}
-		s := strings.TrimSpace(string(b))
-		index := strings.Index(s, "=")
-		if index < 0 {
-			continue
-		}
-
-		key := strings.TrimSpace(s[:index])
-		if len(key) == 0 {
-			continue
-		}
-		value := strings.TrimSpace(s[index+1:])
-		if key == "sql"{
-			cfg.SQL = value
-		}
-		if key == "port" {
-			cfg.Port,_ = strconv.Atoi(value)
-		}
-	}
 }
