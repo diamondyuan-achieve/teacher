ce183700cc3ea8004a4ad3c40f470a9d9d6a78f8
袁凡迪 <541832074@qq.com>
2017-09-20 03:28:07 +0800

    no message

--- sql/torrent.go
+++ sql/torrent.go
@@ -32,6 +32,7 @@ func SaveData(hash []byte, content []byte, torrent []byte) {
 
 
 func SaveHash(unsavedRows []HashIp) {
+	fmt.Println(len(unsavedRows))
 	valueStrings := make([]string, 0, len(unsavedRows))
 	valueArgs := make([]interface{}, 0, len(unsavedRows) * 2)
 	for _, post := range unsavedRows {
